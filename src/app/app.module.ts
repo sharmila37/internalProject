acd0e1b17771b15a955f1b41bd800983da432837
sharmila37 <jsharmil37@gmail.com>
2023-09-06 11:35:11 +0530

    init changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    UserComponent,
+    AddEmployeeComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
